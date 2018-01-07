6a8e540cede2533720ac83e9538c5b4999b9d189
NickAcPT <32451103+NickAcPT@users.noreply.github.com>
2018-01-07 02:35:12 +0000

    Fix emulator check

--- app/src/main/java/net/nickac/buttondeck/MainActivity.java
+++ app/src/main/java/net/nickac/buttondeck/MainActivity.java
@@ -64,11 +64,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public static boolean isEmulator() {
+        return Build.FINGERPRINT.startsWith("generic")
+                || Build.FINGERPRINT.startsWith("unknown")
+                || Build.MODEL.contains("google_sdk")
+                || Build.MODEL.contains("Emulator")
+                || Build.MODEL.contains("Android SDK built for x86")
+                || Build.MANUFACTURER.contains("Genymotion")
+                || (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
+                || "google_sdk".equals(Build.PRODUCT);
+    }
+
     private void scanDevices() {
-        if (Build.PRODUCT.contains("sdk")) {
+        if (isEmulator()) {
             // Emulator
             Intent intent = new Intent(this, ButtonDeckActivity.class);
-            intent.putExtra(ButtonDeckActivity.EXTRA_IP, "10.0.202");
+            intent.putExtra(ButtonDeckActivity.EXTRA_IP, "10.0.2.2");
             startActivity(intent);
             return;
         }
